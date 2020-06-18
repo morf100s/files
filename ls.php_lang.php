2d7e6cb176df9b04541afe453979d5f86a6740b7
morf100s <56072965+morf100s@users.noreply.github.com>
2020-06-19 00:43:55 +0300

    Update ls.php_lang.php

--- ls.php_lang.php
+++ ls.php_lang.php
@@ -1,3 +1,6 @@
 <?php
-echo exec('ls -l');
+// Если включены строгие типы, то есть объявлено (strict_types=1);
+// $file = file_get_contents('./en_lang.php', true);
+// Иначе
+$file = file_get_contents('./en_lang.php', FILE_USE_INCLUDE_PATH);
 ?>
