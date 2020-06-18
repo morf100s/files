e646ec2d0ed5ef10081abccf52299fd405801738
morf100s <56072965+morf100s@users.noreply.github.com>
2020-06-19 00:48:23 +0300

    dd
    dsd

--- ls.php_lang.php
+++ ls.php_lang.php
@@ -1,6 +1,6 @@
 <?php
 // Если включены строгие типы, то есть объявлено (strict_types=1);
-// $file = file_get_contents('./en_lang.php', true);
+$file = file_get_contents('./en_lang.php', true);
 // Иначе
-$file = file_get_contents('./en_lang.php', FILE_USE_INCLUDE_PATH);
+//$file = file_get_contents('./en_lang.php', FILE_USE_INCLUDE_PATH);
 ?>
